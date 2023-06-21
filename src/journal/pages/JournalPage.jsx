d826faf7ed8c6c647c844d8b948077d4e22bcf41
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-21 16:49:29 -0500

    button create created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,31 @@
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { NoteView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
             {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ex voluptates sequi hic ab accusantium reprehenderit nesciunt quas vero labore perferendis quae dolore animi, libero quisquam, ratione quam debitis doloribus?</Typography> */}
             
-            {/* <NothingSelectedView /> */}
+            <NothingSelectedView />
 
-            <NoteView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }}></AddOutlined>
+            </IconButton>
 
         </JournalLayout>
   )
