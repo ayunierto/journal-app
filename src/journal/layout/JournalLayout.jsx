e1803608028a1fa2113be4077cae15be3a5a5a83
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-21 15:00:05 -0500

    created JournalLayout

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material"
+
+const drawerWidth = 240;
+
+// eslint-disable-next-line react/prop-types
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+        
+        {/* NavBar  drawerWidth */}
+
+        {/* SideBar drawerWidth */}
+
+        <Box 
+            component='main'
+            sx={{ flexGrow: 1, p: 3 }}
+        >
+            {/* ToolBar */}
+
+            { children }
+        </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <Typography variant="h1">JournalPage</Typography>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ex voluptates sequi hic ab accusantium reprehenderit nesciunt quas vero labore perferendis quae dolore animi, libero quisquam, ratione quam debitis doloribus?</Typography>
+    </JournalLayout>
   )
 }
