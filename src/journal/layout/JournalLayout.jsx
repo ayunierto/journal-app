a5519427c6753e855e69ec688fcb7d9bdda05016
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-21 15:21:58 -0500

    NavBar created

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material"
+import { NavBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -7,7 +8,7 @@ export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
         
-        {/* NavBar  drawerWidth */}
+        <NavBar drawerWidth={ drawerWidth } />
 
         {/* SideBar drawerWidth */}
 
