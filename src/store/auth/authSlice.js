beea11f042977281f9a44ce97d2dbd94dc335165
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-23 06:10:43 -0500

    config authSlice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        status: 'cheking', // not-authenticated, authenticated
+        uid: null,
+        email: null,
+        displayName: null,
+        photoURL: null,
+        errorMessage: null
+
+    },
+    reducers: {
+        login: (state, action ) => {
+            
+        },
+        logout: ( state, payload ) => {
+
+        },
+        chekingCredentials: ( state ) => {
+
+        }
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { login, logout, chekingCredentials } = authSlice.actions;
