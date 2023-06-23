14b461640cf124c6e6063f07b6e2e7ac45a2855d
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-23 07:57:09 -0500

    google signIn

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from "../../firebase/providers"
 import { chekingCredentials } from "./authSlice"
 
 export const checkingAuthentication = ( email, password ) => {
@@ -12,6 +13,9 @@ export const startGoogleSignIn = ( ) => {
     return async ( dispatch ) => {
 
         dispatch( chekingCredentials() )
+
+        const result = await signInWithGoogle()
+        console.log(result)
 
     }
 }
