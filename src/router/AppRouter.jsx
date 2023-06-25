8ea2fb4d59c2bbce27f74cdce0975c862c00c17a
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-25 18:44:50 -0500

    mantener el estado de autenticacion al recargar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,28 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { CheckingAuth } from "../ui/components/CheckingAuth"
+import { useEffect } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { FirebaseAuth } from "../firebase/config"
+import { login, logout } from "../store/auth/authSlice"
 
 export const AppRouter = () => {
 
     const { status } = useSelector( state => state.auth )
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+      onAuthStateChanged( FirebaseAuth, async(user) => {
+        if (!user) return dispatch( logout() )
+
+        const { uid, email, photoURL, displayName } = user
+        dispatch( login( uid, email, photoURL, displayName ) )
+      })
+    
+    }, [])
+    
 
     if (status === 'checking') {
         return <CheckingAuth />
@@ -15,11 +31,19 @@ export const AppRouter = () => {
     return (
         <Routes>
 
+            {
+                (status === 'authenticate')
+                ? <Route path="/auth/*" element={ <AuthRoutes /> } />
+                : <Route path="/*" element={ <JournalRoutes /> } />
+            }
+
+            <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+
             {/* Login and Register */}
-            <Route path="/auth/*" element={ <AuthRoutes /> } />
+            {/* <Route path="/auth/*" element={ <AuthRoutes /> } /> */}
 
             {/* JournalApp */}
-            <Route path="/*" element={ <JournalRoutes /> } />
+            {/* <Route path="/*" element={ <JournalRoutes /> } /> */}
         </Routes>
     )
 }
